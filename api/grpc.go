4e51b87475b20cf29ac32549170bb107a63b50b5
AlexSkilled <olegthander@yandex.ru>
2024-03-19 22:38:16 +0300

    [master] removed gateway port

--- api/grpc.go
+++ api/grpc.go
@@ -8,22 +8,19 @@ const (
 	GRPSServerType  = "grpc"
 	DefaultGrpcPort = 50051
 
-	EnvVarGRPCPort        = "GRPC_PORT"
-	EnvVarGRPCGatewayPort = "GRPC_GATEWAY_PORT"
+	EnvVarGRPCPort = "GRPC_PORT"
 )
 
 type GRPC struct {
 	Name
 
-	Port        uint16 `yaml:"port"`
-	GatewayPort uint16 `yaml:"gateway_port"`
+	Port uint16 `yaml:"port"`
 }
 
 func (g *GRPC) ToEnv() map[string]string {
 	return map[string]string{
-		EnvVarGRPCPort:        strconv.FormatUint(uint64(g.Port), 10),
-		EnvVarGRPCGatewayPort: strconv.FormatUint(uint64(g.GatewayPort), 10),
-		EnvServerName:         g.GetName(),
+		EnvVarGRPCPort: strconv.FormatUint(uint64(g.Port), 10),
+		EnvServerName:  g.GetName(),
 	}
 }
 
@@ -34,13 +31,6 @@ func (g *GRPC) FromEnv(in map[string]string) error {
 	}
 
 	g.Port = uint16(portUint)
-
-	gwPortUint, err := strconv.ParseUint(in[EnvVarGRPCGatewayPort], 10, 16)
-	if err != nil {
-		return err
-	}
-
-	g.GatewayPort = uint16(gwPortUint)
 
 	if g.Name == "" {
 		g.Name = GRPSServerType
@@ -59,14 +49,5 @@ func (g *GRPC) GetPort() uint16 {
 
 func (g *GRPC) GetPortStr() string {
 	p := g.GetPort()
-	return strconv.FormatUint(uint64(p), 10)
-}
-
-func (g *GRPC) GetGatewayPort() uint16 {
-	return g.GatewayPort
-}
-
-func (g *GRPC) GetGatewayPortStr() string {
-	p := g.GatewayPort
 	return strconv.FormatUint(uint64(p), 10)
 }
